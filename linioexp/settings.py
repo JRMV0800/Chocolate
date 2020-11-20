bdf9ebd0f379be3deef3994d3f6592081c9c4968
JRMV0800 <jr.mestanzav@alum.up.edu.pe>
2020-11-19 20:15:56 -0500

    2

--- linioexp/settings.py
+++ linioexp/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- main/forms.py
+++ main/forms.py
@@ -5,13 +5,13 @@ from .models import Localizacion, Categoria
 
 class UserForm(UserCreationForm):
     # django.contrib.auth.User attributes
-    first_name = forms.CharField(max_length=150, required=False)
-    last_name = forms.CharField(max_length=150, required=False)
+    primer_nombre = forms.CharField(max_length=150, required=False)
+    apellidos = forms.CharField(max_length=150, required=False)
     email = forms.EmailField(max_length=150)
 
     # Profile attributes
-    documento_identidad = forms.CharField(max_length=8)
-    fecha_nacimiento = forms.DateField()
+    DNI = forms.CharField(max_length=8)
+    fecha_de_nacimiento = forms.DateField()
     estado = forms.CharField(max_length=3)
     ## Opciones de genero
     MASCULINO = 'MA'
@@ -25,27 +25,26 @@ class UserForm(UserCreationForm):
     genero = forms.ChoiceField(choices=GENERO_CHOICES)
 
     # Cliente attributes
-    is_cliente = forms.BooleanField(required=False)
     preferencias = forms.ModelChoiceField(queryset=Categoria.objects.all(), required=False)
 
-    # Colaborador attributes
-    is_colaborador = forms.BooleanField(required=False)
-    reputacion = forms.FloatField(required=False)
-    cobertura_entrega = forms.ModelChoiceField(queryset=Localizacion.objects.all(), required=False)
+
 
     class Meta:
         model = User
         fields = ['username',
-        'first_name',
-        'last_name',
+        'primer_nombre',
+        'apellidos',
         'email',
-        'documento_identidad',
-        'fecha_nacimiento',
+        'DNI',
+        'fecha_de_nacimiento',
         'estado',
         'genero',
-        'is_cliente',
         'preferencias',
-        'is_colaborador',
-        'reputacion',
-        'cobertura_entrega',
         ]
+
+
+
+
+
+
+
